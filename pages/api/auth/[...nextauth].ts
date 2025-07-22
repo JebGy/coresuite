bf787910e378feab1b89a821568818d3bd2d4df9
JebGy <martinmunayco123@hotmail.com>
2025-07-22 06:47:42 -0500

    feat:pagos

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -56,7 +56,7 @@ export default NextAuth({
         // Validar contraseña
         if (!user.password) {
           // Permitir acceso solo al root inicial (sin contraseña)
-          if (user.email === 'root@admin.com') {
+          if (user.email === 'root@admin.com' && user.password) {
             await registrarLog({
               usuarioId: user.id,
               accion: 'LOGIN',
