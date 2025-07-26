a35d3840a8ff713e41345c653b6755a0e47c73d7
JebGy <martinmunayco123@hotmail.com>
2025-07-26 09:00:46 -0500

    feat: tests & api

--- pages/api/movimientos.ts
+++ pages/api/movimientos.ts
@@ -1,54 +1,33 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client';
-import { calcularKardex } from '../../lib/kardex';
-import { Movimiento } from '@/types';
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+import { calcularKardex } from "../../lib/kardex";
+import { Movimiento } from "@/types";
 const prisma = new PrismaClient();
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    const { productoId } = req.query;
-    // Convertir productoId a número
-    const productoIdInt = parseInt(productoId as string, 10);
-    
-    // Adaptar el tipo de fecha a string para cumplir con el tipo Movimiento
-    const movimientosRaw = await prisma.movimiento.findMany({
-      where: { productoId: productoIdInt },
-      orderBy: { fecha: 'asc' }
-    });
-    // Solución: Convertir null a undefined para 'precioUnitario' y 'factura', y null a undefined para 'ordenEntregaId'
-    const movimientos: Movimiento[] = movimientosRaw.map((m) => ({
-      id: m.id,
-      tipo: m.tipo as "entrada" | "salida",
-      fecha: m.fecha.toISOString().slice(0, 10),
-      cantidad: m.cantidad,
-      precioUnitario: m.precioUnitario === null ? undefined : m.precioUnitario,
-      motivo: m.motivo,
-      factura: m.factura === null ? undefined : m.factura,
-      productoId: m.productoId,
-      almacenId: m.almacenId,
-      ordenEntregaId: m.ordenEntregaId === null ? undefined : m.ordenEntregaId,
-    }));
-    // Calcula el Kardex
-    const kardex = calcularKardex(movimientos );
-    return res.json(kardex);
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
   }
-  if (req.method === 'POST') {
-    const { tipo, fecha, cantidad, precioUnitario, motivo, productoId, almacenId } = req.body;
-    
+  if (req.method === "POST") {
+    const { tipo, cantidad, precioUnitario, productoId, almacenId } = req.body;
+
     // Convertir IDs a números
     const productoIdInt = parseInt(productoId, 10);
     const almacenIdInt = parseInt(almacenId, 10);
-    
+
     const movimiento = await prisma.movimiento.create({
       data: {
         tipo,
-        fecha: new Date(fecha),
+        fecha: new Date(),
         cantidad,
         precioUnitario,
-        motivo,
+        motivo: "REGISTRO INICIAL",
         producto: { connect: { id: productoIdInt } },
         almacen: { connect: { id: almacenIdInt } },
-      }
+      },
     });
     return res.status(201).json(movimiento);
   }
