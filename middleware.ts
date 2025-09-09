7e2f11e7cf02beaa4231a4dd740da836ee8565b2
JebGy <martinmunayco123@hotmail.com>
2025-09-09 10:07:44 -0500

    FEAT: Visuales y nuevos pricing

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,15 @@ import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const session = req.cookies.get('session')?.value;
+  const pathname = req.nextUrl.pathname;
   
   // Si no hay sesión y el usuario intenta acceder a rutas protegidas
   if (!session && (req.nextUrl.pathname.startsWith('/application') || 
                   req.nextUrl.pathname.startsWith('/ordenes-entrega') ||
                   req.nextUrl.pathname.startsWith('/trabajadores') ||
                   req.nextUrl.pathname.startsWith('/traslados') ||
-                  req.nextUrl.pathname.startsWith('/unidades'))) {
+                  req.nextUrl.pathname.startsWith('/unidades')) &&
+                  !req.nextUrl.pathname.startsWith('/registro-proveedor')) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 }
